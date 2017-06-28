608aeb8c76a2206a07d3ae5faa3c229177485a62
earthgee <865603556@qq.com>
2017-06-28 17:05:48 +0800

    finish Multidex

--- Multidex/mutlidex/src/main/java/com/earthgee/mutlidex/Multidex.java
+++ Multidex/mutlidex/src/main/java/com/earthgee/mutlidex/Multidex.java
@@ -12,10 +12,15 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
+import java.util.zip.ZipFile;
+
+import dalvik.system.DexFile;
 
 /**
  * Created by zhaoruixuan on 2017/6/19.
@@ -66,6 +71,14 @@ public class Multidex {
                 List<File> files=MultiDexExtractor.load(context,applicationInfo,dexDir,false);
                 if(checkValidZipFiles(files)){
                     installSecondaryDexes(loader,dexDir,files);
+                }else{
+                    files=MultiDexExtractor.load(context,applicationInfo,dexDir,true);
+
+                    if(checkValidZipFiles(files)){
+                        installSecondaryDexes(loader,dexDir,files);
+                    }else{
+                        throw new RuntimeException("Zip files were not valid.");
+                    }
                 }
             }
         }catch (Exception e){
@@ -180,11 +193,95 @@ public class Multidex {
                 Arrays.asList(parameterTypes)+" not found in "+instance.getClass());
     }
 
+    private static void expandFieldArray(Object instance,String fieldName,Object[] extraElements) throws Exception{
+        Field jlrField=findField(instance,fieldName);
+        Object[] original= (Object[]) jlrField.get(instance);
+        Object[] combined= (Object[]) Array.newInstance(original.getClass().getComponentType(),original.length+extraElements.length);
+        System.arraycopy(original,0,combined,0,original.length);
+        System.arraycopy(extraElements,0,combined,original.length,extraElements.length);
+        jlrField.set(instance,combined);
+    }
+
     private static final class V19{
         private static void install(ClassLoader loader,List<File> additionalClassPathEntries,File optimizedDirectory) throws Exception{
-                        
+            Field pathListField=findField(loader,"pathList");
+            Object dexPathList=pathListField.get(loader);
+            ArrayList<IOException> suppressedExceptions=new ArrayList<>();
+            expandFieldArray(dexPathList,"dexElements",makeDexElements(dexPathList,new ArrayList<File>(additionalClassPathEntries)
+                    ,optimizedDirectory,suppressedExceptions));
+            if(suppressedExceptions.size()>0){
+                Field suppressedExceptionsField=findField(dexPathList,"dexElementsSuppressedExceptions");
+                IOException[] dexElementsSuppressedExceptions= (IOException[]) suppressedExceptionsField.get(dexPathList);
+
+                if(dexElementsSuppressedExceptions==null){
+                    dexElementsSuppressedExceptions=suppressedExceptions.toArray(new IOException[suppressedExceptions.size()]);
+                }else{
+                    IOException[] combined=new IOException[suppressedExceptions.size()+dexElementsSuppressedExceptions.length];
+                    suppressedExceptions.toArray(combined);
+                    System.arraycopy(dexElementsSuppressedExceptions,0,combined,
+                            suppressedExceptions.size(),dexElementsSuppressedExceptions.length);
+                    dexElementsSuppressedExceptions=combined;
+                }
+
+                suppressedExceptionsField.set(dexPathList,dexElementsSuppressedExceptions);
+            }
+        }
+
+        private static Object[] makeDexElements(Object dexPathList,ArrayList<File> files
+                    , File optimizedDirectory,ArrayList<IOException> suppressedExceptions) throws Exception{
+            Method makeDexElements=findMethod(dexPathList,"makeDexElements",ArrayList.class,File.class,ArrayList.class);
+
+            return (Object[]) makeDexElements.invoke(dexPathList,files,optimizedDirectory,suppressedExceptions);
+        }
+
+    }
+
+    private static final class V14{
+
+        private static void install(ClassLoader loader,List<File> additionalClassPathEntries,File optimizeDirectory) throws Exception{
+            Field pathListField=findField(loader,"pathList");
+            Object dexPathList=pathListField.get(loader);
+            expandFieldArray(dexPathList,"dexElements",makeDexElements(dexPathList,new ArrayList<File>(additionalClassPathEntries),optimizeDirectory));
+        }
+
+        private static Object[] makeDexElements(Object dexPathList,ArrayList<File> files,File optimizedDirectory) throws Exception{
+            Method makeDexElements=findMethod(dexPathList,"makeDexElements",ArrayList.class,File.class);
+            return (Object[]) makeDexElements.invoke(dexPathList,files,optimizedDirectory);
+        }
+
+    }
+
+    private static final class V4{
+        private static void install(ClassLoader loader,List<File> additionalClassPathEntries) throws Exception{
+            int extraSize=additionalClassPathEntries.size();
+
+            Field pathField=findField(loader,"path");
+
+            StringBuilder path=new StringBuilder((String)pathField.get(loader));
+            String[] extraPaths=new String[extraSize];
+            File[] extraFiles=new File[extraSize];
+            ZipFile[] extraZips=new ZipFile[extraSize];
+            DexFile[] extraDexs=new DexFile[extraSize];
+            for(ListIterator<File> iterator=additionalClassPathEntries.listIterator();iterator.hasNext();){
+                File additionalEntry=iterator.next();
+                String entryPath=additionalEntry.getAbsolutePath();
+                path.append(":").append(entryPath);
+                int index=iterator.previousIndex();
+                extraPaths[index]=entryPath;
+                extraFiles[index]=additionalEntry;
+                extraZips[index]=new ZipFile(additionalEntry);
+                extraDexs[index]=DexFile.loadDex(entryPath,entryPath+".dex",0);
+            }
+
+            pathField.set(loader,path.toString());
+            expandFieldArray(loader,"mPaths",extraPaths);
+            expandFieldArray(loader,"mFiles",extraFiles);
+            expandFieldArray(loader,"mZips",extraZips);
+            expandFieldArray(loader,"mDexs",extraDexs);
         }
     }
+
+
 
 }
 
