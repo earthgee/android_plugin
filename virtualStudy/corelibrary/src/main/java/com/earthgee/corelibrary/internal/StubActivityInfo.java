d0afe89cdcb6cd0c9e3c60b11554ab000377b9a8
earthgee <865603556@qq.com>
2017-07-31 19:17:50 +0800

    占坑activi他、y

--- virtualStudy/corelibrary/src/main/java/com/earthgee/corelibrary/internal/StubActivityInfo.java
+++ virtualStudy/corelibrary/src/main/java/com/earthgee/corelibrary/internal/StubActivityInfo.java
@@ -41,25 +41,62 @@ public class StubActivityInfo {
         });
         boolean windowIsTranslucent=array.getBoolean(0,false);
         array.recycle();
-        stubActivity=String.format(STUB_ACTIVITY_STANDARD,corePackage,usedSingleInstanceStubActivity);
+        stubActivity=String.format(STUB_ACTIVITY_STANDARD,corePackage,usedStandardStubActivity);
         switch (launchMode){
             case ActivityInfo.LAUNCH_MULTIPLE:{
-
+                stubActivity=String.format(STUB_ACTIVITY_STANDARD,corePackage,usedStandardStubActivity);
+                if(windowIsTranslucent){
+                    stubActivity=String.format(STUB_ACTIVITY_STANDARD,corePackage,2);
+                }
+                break;
             }
             case ActivityInfo.LAUNCH_SINGLE_TOP:{
-
+                usedSingleTopStubActivity=usedSingleTopStubActivity%MAX_COUNT_SINGLETOP+1;
+                stubActivity=String.format(STUB_ACTIVITY_SINGLETOP,corePackage,usedSingleTopStubActivity);
+                break;
             }
             case ActivityInfo.LAUNCH_SINGLE_TASK:{
-
+                usedSingleTaskStubActivity=usedSingleTaskStubActivity%MAX_COUNT_SINGLETASK+1;
+                stubActivity=String.format(STUB_ACTIVITY_SINGLETASK,corePackage,usedSingleTaskStubActivity);
+                break;
             }
             case ActivityInfo.LAUNCH_SINGLE_INSTANCE:{
-
+                usedSingleInstanceStubActivity=usedSingleInstanceStubActivity%MAX_COUNT_SINGLEINSTANCE+1;
+                stubActivity=String.format(STUB_ACTIVITY_SINGLEINSTANCE,corePackage,usedSingleInstanceStubActivity);
+                break;
             }
             default:break;
         }
+
+        mCachedStubActivity.put(className,stubActivity);
+        return stubActivity;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
