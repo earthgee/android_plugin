d947d42eb27f7845c812af28ebe8e8cafea1c525
zhaoruixuan <865603556@qq.com>
2019-12-11 11:32:42 +0800

    activity stub

---
+++ ApkProxy/pluginlib/src/main/java/com/earthgee/pluginlib/stub/ActivityStub.java
@@ -0,0 +1,28 @@
+package com.earthgee.pluginlib.stub;
+
+import android.app.Activity;
+
+/**
+ * Created by zhaoruixuan on 2019/12/11.
+ */
+public abstract class ActivityStub extends Activity {
+
+    private static class StandardStub extends ActivityStub{
+
+    }
+
+    private static class SingleTopStub extends ActivityStub{
+
+    }
+
+    private static class SingleTaskStub extends ActivityStub{
+
+    }
+
+    private static class SIngleInstanceStub extends ActivityStub{
+
+    }
+
+
+
+}
